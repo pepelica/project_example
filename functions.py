9dd6160e83002f5362a4b869519c70fb6d5a4eea
unknown <aleksandra-korenskaya@mail.ru>
2020-11-13 00:16:52 +0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,178 @@
+from Bio import SeqIO
+from Bio.Seq import Seq, UnknownSeq, Alphabet
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio.SeqRecord import SeqRecord
+import pandas as pd
+from subprocess import Popen, PIPE
+import os
+
+
+
+
+
+def merge(records, path):
+    """Merge multiple SeqRecords into one, using a defined spacer
+
+    :param records: Iterable containing SeqRecords to be merged
+    :param length: Length of the spacer in kbp
+    :param spacer: Kind of spacer to use ('n' for UnknownSeq spacer, 'stop' for all-frame stop codon spacer)
+
+    :return: A single SeqRecord that is the product of the merge.
+    """
+    length=20
+    spacer='n'
+    ALL_FRAME_STOP_MOTIF = 'TAGCTAACTGACCGTCAGTTAGCTA'
+
+    if not len(records):
+        raise ValueError("No records given")
+
+    if spacer == 'stop':
+        spacer_seq = Seq(ALL_FRAME_STOP_MOTIF * 40 *
+                         length, Alphabet.generic_dna)
+    else:
+        spacer_seq = UnknownSeq(
+            length * 1000, alphabet=Alphabet.generic_dna, character='N')
+
+    new_rec = records[0]
+
+    if len(records) == 1:
+        with open(path, 'w') as merged_file:
+           SeqIO.write(new_rec, merged_file, 'genbank')
+    else:  
+        rec_id = new_rec.id
+        rec_name = new_rec.name
+        rec_desc = new_rec.description
+        date = new_rec.annotations.get('date', '')
+        source = new_rec.annotations.get("source", '')
+        organism = new_rec.annotations.get('organism', '')
+        taxonomy = new_rec.annotations.get('taxonomy', [])
+        data_file_division = new_rec.annotations.get('data_file_division', 'UNK')
+        topology = new_rec.annotations.get('topology', 'linear')
+    
+        for i, rec in enumerate(records[1:]):
+            spacer_id = 'spacer_{}'.format(i + 1)
+    
+            spacer_feature = SeqFeature(FeatureLocation(0, length * 1000, 0),
+                                        type='misc_feature', id=spacer_id,
+                                        qualifiers={'note': [spacer_id]})
+    
+            spacer_rec = SeqRecord(spacer_seq, id=spacer_id, name=spacer_id,
+                                   description=spacer_id, features=[spacer_feature])
+    
+            new_rec = new_rec + spacer_rec + rec
+    
+        new_rec.id = rec_id
+        new_rec.name = rec_name
+        new_rec.description = rec_desc
+        new_rec.annotations["date"] = date
+        new_rec.annotations["source"] = source
+        new_rec.annotations["organism"] = organism
+        new_rec.annotations["taxonomy"] = taxonomy
+        new_rec.annotations["data_file_division"] = data_file_division
+        new_rec.annotations["topology"] = topology
+        
+        with open(path, 'w') as merged_file:
+            SeqIO.write(new_rec, merged_file, 'genbank')
+
+
+
+#сюда попадаем, если файл не парсится; создается читаемая версия файла
+def modify_input_file(handle):
+    new_handle = []
+    new_line = ''
+
+    for line in handle:
+#        print(line)
+        new_line = line
+#        print(line)
+        if 'LOCUS       ' in line:
+            len_start = line.find('length_')+7
+            len_end = line.find('_cov')
+            lenght = line[len_start:len_end]
+            indx = line.rfind(lenght)
+            new_line = line[:indx]+'\t'+line[indx:]
+            #new_line = line.replace('.', '\t')
+            if line.count('-')<2:
+
+                new_line = new_line.replace('\n', '\t')
+
+        new_handle.append(new_line)
+
+    return ''.join(new_handle)
+
+        
+                
+#считывает входной файл, если не парсится - отправляет на корректировку, пересохраняет и парсит, возвращает список геномных карточек образца                
+def read_input_file(path, new_handle_file):
+
+    contents = os.listdir(path)
+
+    records = []
+    if len(contents) == 1:
+
+        try:
+            with open(path+'//'+ contents[0], "r") as handle:
+                for record in SeqIO.parse(handle, "genbank"):
+                    records.append(record)
+        except:
+            with open(path+'//'+ contents[0], "r") as handle:
+                new_handle = modify_input_file(handle)
+                with open(new_handle_file, 'w') as file:
+                    file.write(new_handle)
+
+
+            for record in SeqIO.parse(new_handle_file, "genbank"):
+                records.append(record)
+    elif not len(contents):
+        raise ValueError
+                    
+    else:
+        for genome in contents:
+            with open(path + '//'+genome, 'rU') as handle:
+                record = SeqIO.parse(handle, 'genbank')
+                records.append(record)
+    return records
+
+#запускает программу eloe
+def run_EloE(EloE_input_path, EloE_output_path, eloe_path):
+
+    proc = Popen(
+        ['java', '-jar', eloe_path,  
+         EloE_input_path, EloE_output_path],
+        shell=True,
+        stdout=PIPE, stderr=PIPE)
+    proc.wait()    # дождаться выполнения
+
+    try:
+        proc.communicate(timeout=15)       
+        return True
+    except Popen.TimeoutExpired:
+        proc.kill()
+        return False
+    except:
+
+        return False
+        
+
+#находит файл eei и разбивает его на заданное количество частей
+def find_quantile(EloE_output_path, output, quantile):
+    EloEresults = os.listdir(EloE_output_path)
+    for EloE_file in EloEresults:
+        if '_eei' in EloE_file:
+            df = pd.read_csv(EloE_output_path+'\\'+EloE_file, delimiter='\t', 
+                                   usecols = ['locus_tag', 'protein_id', 'gene_id',
+                                              'gene_name', 'COG', 'EEI'])
+            start = 0
+            end = 0
+            for i in range(quantile):
+                start = end
+                end = len(df)-round(len(df)/4*(quantile - 1-i))
+#                df1 = df.iloc[round(i*len(df)/4):len(df)-round(len(df)/4*(quantile - 1-i)), :]
+                df1 = df.iloc[start:end, :]
+
+                df1.to_csv(f'{output}_quantile{i+1}.txt', sep = '\t', na_rep = '-')
+            break
+
+
+
+
---
+++ main.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 14:30:52 2020
+
+@author: Aleksandra
+"""
+
+
+import os
+from argparse import ArgumentParser
+import shutil
+import time
+import functions as fu
+import configparser
+
+
+quantile = 4
+ 
+def main(quantile):
+    
+    parser = ArgumentParser(description = "Find quantiles for proteins expression in gbk files.")
+    
+    parser.add_argument('-i', '--indir', type = str,
+                        help='A directory containing folders with gbk files.')
+    parser.add_argument('-o', '--outdir', type = str,
+                        help="A directory for result files.")
+    parser.add_argument('-q', '--quantiles', type = int, default = 4,
+                        help="Number of quartiles (default: %(default)s).")
+
+
+    args = parser.parse_args()
+    print(args)
+    
+    #read ini file
+    config = configparser.ConfigParser()    
+    config.read('path.ini')
+    eloe_path = config['PATH']['eloe_path']
+    input_ini_path = config['PATH']['input_path']
+    output_ini_path = config['PATH']['output_path']
+    
+    
+    input_path = args.indir
+    output_path = args.outdir
+    quantile = args.quantiles
+    
+    #если пути не введены в командную строку, берутся пути из ini файла
+    if not input_path:
+        input_path = input_ini_path
+    if not output_path:
+        output_path = output_ini_path
+
+    try:                                    
+        samples = os.listdir(input_path)    #проверка, что входной путь корректен
+    except(FileNotFoundError):
+        print('Please provide a command line parameter using --indir or -i or add input path to the path.ini file')
+        return
+    
+    #удаляем все файлы в рабочей директории
+    try:                                    #проверка, что выходной путь корректен
+        contents = os.listdir(output_path)
+    except(FileNotFoundError):
+        print('Please provide a command line parameter using --outdir or -o or add output path to the path.ini file')
+        return
+    
+    if not os.path.exists(eloe_path):
+        print('Please add correct eloe path to the path.ini file')
+        return
+        
+    if len(contents)!= 0:
+        for file in contents:
+            shutil.rmtree(output_path + '\\'+file)
+            
+    
+    #Создаем общую папку для результата подсчета кванитлей
+    quantile_output_path = output_path+'\\quantile\\'
+     
+    os.mkdir(quantile_output_path)
+    
+
+
+    for sample in samples:
+        
+        path = input_path+ '\\' + sample
+        org_temp_res_path = output_path + '\\temp\\'+ sample
+        
+        
+        gbk_files = org_temp_res_path + '\\gbk\\'+sample
+        try:
+            os.makedirs(gbk_files)
+        except:
+            shutil.rmtree(output_path + '\\temp\\'+sample)
+            os.makedirs(gbk_files)  
+            
+        try:
+
+            #читаем файл, если некорректен - преобразуем, сохраняем в новый файл
+            #возвращает список геномных карточек
+            records = fu.read_input_file(path, gbk_files+'\\normal.gbk') #read and modificate file for parsing
+            print(records)
+        except ValueError:
+            print(sample + ' is empty')
+                  
+        except:       
+            print(sample +' have incorrect file format')
+        
+        else:
+            #cклеиваем геномные карточки в одну, записываем в файл
+            fu.merge(records, gbk_files+'\\merged.gbk') #merge records to one record 
+
+                
+            #если исправлялся некорректный файл - удаляем полученный файл
+            #(чтобы в папке, подаваемой елое был только один файл)
+            try:
+                os.remove(gbk_files+'\\normal.gbk')
+            except:
+                print('file was not modified')
+             
+            #создаем папку с результатами елое этого организма
+            eloe_res_path = org_temp_res_path+'\\eloe\\'
+            os.makedirs(eloe_res_path)
+             
+            #запускаем елое, записываем результаты для организма
+            eloe_res = fu.run_EloE(org_temp_res_path + '\\gbk', eloe_res_path, eloe_path)
+            
+            #папка, куда записываются файлы с квантилями этого организма 
+            sample_quantile = quantile_output_path + '\\'+ sample+'\\'
+            os.mkdir(sample_quantile) 
+            if eloe_res:
+                fu.find_quantile(eloe_res_path+sample.replace(' ', '_'), sample_quantile + sample, quantile)
+                print('Found quantile for sample '+sample)
+            else: 
+                print ("EloE can't finish the process for "+sample)
+            
+        
+       
+
+    
+    #удаляет рабочую папку с gbk файлами и результатами eloe
+    shutil.rmtree(output_path + '\\temp')
+        
+
+
+
+main(quantile)
